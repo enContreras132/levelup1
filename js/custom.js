f8da6244dcc07dccb0e1f40a77cd28b3ad055853
Enzo <en.contrerase@duocuc.cl>
2025-09-26 01:49:26 -0300

    validaciones, cambios generales y un poco de limpieza de codigo

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,4 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getYear();
-
-
+window.addEventListener('load', ()=>{
 // isotope js
 $(window).on('load', function () {
     $('.filters_menu li').click(function () {
@@ -34,14 +25,6 @@ $(document).ready(function() {
     $('select').niceSelect();
   });
 
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
 
 // client section owl carousel
 $(".client_owl-carousel").owlCarousel({
@@ -67,4 +50,54 @@ $(".client_owl-carousel").owlCarousel({
             items: 2
         }
     }
+});
+
+const form = document.getElementById('formulario')
+const nombre = document.getElementById('nombre')
+const telefono = document.getElementById('telefono')
+const mail = document.getElementById('mail')
+const rut = document.getElementById('rut')
+const nacimiento = document.getElementById('nacimiento')
+const direccion = document.getElementById('direccion')
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    validacionCampos()
+})
+
+const validacionCampos = ()=>{
+    const nombreValor = nombre.value.trim()
+    const telefonoValor = telefono.value.trim()
+    const mailValor = mail.value.trim()
+    const rutValor = rut.value.trim()
+    const nacimientoValor = nacimiento.value.trim()
+    const direccionValor = direccion.value.trim()
+    
+    if (nombreValor === ''){
+        validaFalla(nombre, 'campo vacio')
+    } else {
+        validaBien(nombre)        
+    }
+
+    if (telefonoValor === ''){
+        validaFalla(telefono, 'campo vacio')
+    } else {
+        validaBien(telefono)        
+    }  
+    
+}
+
+const validaFalla = (input, msg)=>{
+    const formControlador = input.parentElement
+    const aviso =formControlador.querySelector('p')
+    aviso.innerText = msg
+
+    formControlador.className = 'form-controlador falla'
+}
+
+const validaBien = (input)=>{
+    const formControlador = input.parentElement
+
+    formControlador.className = 'form-controlador bien'
+}
 });
